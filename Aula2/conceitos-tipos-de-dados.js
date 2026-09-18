aa37ced16042cc60afa1ab00228004bbaa147ef4
lucas <lkxinoaowj@gmail.com>
2026-09-18 11:32:50 -0300

    Back-end

--- Aula2/conceitos-tipos-de-dados.js
+++ Aula2/conceitos-tipos-de-dados.js
@@ -11,3 +11,24 @@
 // Null (valor vazio)
 // underfined (quando algo não foi definido)
 
+// STRING (textos)
+// Uma string é um texto, sempre escrito entre aspas ("" ou '')
+let nome = "Jarvis"
+let mensagem = "Mega Brain"
+
+console.log(nome)
+console.log(mensagem)
+
+let saudacao = "Olá " + nome + "!";
+console.log(saudacao); // Exibe "Olá, Jarvis"
+
+// typeof
+// O typeof serve para descobrir o tipo de um valor ou variavel
+
+//let nomeDois = "Bryan";
+//console.log(nomeDois)
+
+let soma = 10+20; 
+console.log(soma)
+
+
---
+++ Aula2/exercicio_01.js
@@ -0,0 +1,14 @@
+//Atividade Number 
+//Crie uma variavel chama anoAtual e outra chamada anoNascimento.
+//Calcule e exiba a idade no(a) console.
+
+const anoAtual = "2026"
+const anoNascimento ="1988"
+
+console.log (anoAtual)
+console.log (anoNascimento)
+
+const soma = anoAtual - anoNascimento
+console.log (soma)
+
+
