943ad93e017ae6f967400242ed399948775cc093
lucas <lkxinoaowj@gmail.com>
2026-09-18 09:57:11 -0300

    Back-end

---
+++ Aula2/conceito-geral.js
@@ -0,0 +1,49 @@
+// Inicio BackEnd - JavaScript
+
+// Variaveis
+
+// Antigo!!!!!!!!, pode ser redeclarado e mudar valor
+
+var idade = 10
+var idade =20 // não gera erro
+
+// Mais moderno, pode mudar de valor, mas não pode ser redeclarado 
+
+let nome = "julia"
+
+nome = "Dafny"; // permitido 
+
+// Se usar "let" denovo, o codigo vai dar erro, pois não precisar ser definido novamente 
+
+// const é fixo, não pode mudar de valor
+const pi = 3.14
+// pi = 40 //erro
+
+
+//------------------------------
+ // Formas de escrever um codigo
+//-----------------------------
+
+
+ // CAMEL CASE ---- * A mais famosa 
+ // - Primeira palavra minuscula 
+ // - Palavras seguintes começam com maiuscula 
+
+ //let nomeCompleto; let idadeUsuario; functioncalcularIdade(){}
+
+//--------------------------------------
+//PASCALCASE
+// - Toda palavra começa com letra maiuscula
+
+//class UsuarioSistema{
+//   constructor(nome,idade){
+//       this.nome =nome;
+//       this.idade =idade;
+//
+//   }
+// };
+//----------------------------------------
+// SNAKE CASE
+// - Palavras separadas por undercore_
+
+// let nome_completo, let total_vendas;
---
+++ Aula2/conceitos-tipos-de-dados.js
@@ -0,0 +1,13 @@
+//Tipos de dados
+// No JavaScript, tudo o que armazenamentos em variáveis tem um tipo.
+//Esses tipos definem o que podemos fazer com os valores.
+
+// Principais tipos de dados:
+// String (textos)
+// Number (numeros)
+// Boolean (verdadeiro ou falso)
+// Object (objetos, que agrupam informações)
+// Array (listas des valores)
+// Null (valor vazio)
+// underfined (quando algo não foi definido)
+
