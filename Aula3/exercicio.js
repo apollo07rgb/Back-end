6eafa629a438ec5332bec1532a95af65a20483e5
lucas <lkxinoaowj@gmail.com>
2026-09-25 11:34:37 -0300

    BACK-END

---
+++ Aula3/exercicio.js
@@ -0,0 +1,208 @@
+// Exercício 1 – Sistema de Login
+//Crie um objeto usuario com:
+// login
+// senha
+// tipo
+//Depois, exiba a mensagem:
+//O login do administrador é admin e a senha é 1234, muito fraca por sinal.
+
+//---------------------------------------
+let Sistema = {
+
+    login: "adm",
+    senha: "123",
+    tipo: "fraca"
+}
+
+console.log (`O login do administrador é ${Sistema.login} e a senha é ${Sistema.senha}, muito ${Sistema.tipo} por sinal.`)
+
+//-----------------------------------------
+
+
+//Exercício 2 – Cadastro de Aluno
+//Crie um objeto aluno com:
+// -nome
+// -curso
+// -turma
+//Depois, exiba a mensagem:
+//O aluno Lucas está matriculado no curso de Desenvolvimento de Sistemas, na turma 2º DS.
+
+//------------------------------------------
+
+let Cadastro = {
+
+    nome: "Lucas",
+    curso: "DS",
+    turma: "2 DS"
+}
+
+console.log (`O aluno ${Cadastro.nome} está matriculado no curso de ${Cadastro.curso}, na turma ${Cadastro.turma}.
+`)
+
+//---------------------------------------------
+
+//Exercício 3 – Professor
+//Crie um objeto professor com:
+// -nome
+// -disciplina
+// -turno
+//Depois, exiba a mensagem:
+//O professor Carlos ministra a disciplina de JavaScript no turno da manhã.
+
+
+//-----------------------------------------------
+
+let Professor = {
+
+    nome: "Carlos",
+    disciplina: "JavaScript",
+    turno: "Manhã"
+}
+
+    console.log (`O professor ${Professor.nome} ministra a disciplina de ${Professor.disciplina} no turno da ${Professor.turno}.`)
+
+//-------------------------------------------------
+
+//Exercício 4 – Empresa
+//Crie um objeto empresa com:
+// -nome
+// -area
+// -cidade
+//Depois, exiba a mensagem:
+//A empresa Tech Solutions atua na área de Tecnologia e fica em Fortaleza.
+
+//---------------------------------------------------
+
+let Empresa = {
+
+    nome: "Tech Solutions",
+    area: "tecnologia",
+    cidade: "fortaleza"
+}
+
+console.log (`A empresa ${Empresa.nome} atua na área de ${Empresa.area} e fica em ${Empresa.cidade} `)
+//-----------------------------------------------------
+
+
+//Exercício 5 – Computador
+//Crie um objeto computador com:
+// -marca
+// -processador
+// -memoria
+//Depois, exiba a mensagem:
+//O computador da marca Dell possui processador Intel i5 e memória de 8GB.
+
+//------------------------------------------------------
+
+let Computador = {
+
+    marca: "Dell",
+    processador: "intel i5",
+    memoria: "8gb"
+}
+
+console.log (`O computador da marca ${Computador.marca} possui ${Computador.processador} e memoria ${Computador.memoria} `)
+
+//----------------------------------------------------------
+
+//Exercício 6 – Livro
+//Crie um objeto livro com:
+// -titulo
+// -autor
+// -paginas
+//Depois, exiba a mensagem:
+//O livro JavaScript Básico foi escrito por Ana Silva e possui 200 páginas.
+
+//--------------------------------------------------------------
+
+let Livro = {
+
+    titulo: "JavaScript",
+    autor: "Ana Silva",
+    paginas: "200 "
+}
+
+console.log(`O livro ${Livro.titulo} Básico foi escrito por ${Livro.autor} e possui ${Livro.paginas} páginas.`)
+
+//----------------------------------------------------------------
+
+
+//Exercício 7 – Filme
+//Crie um objeto filme com:
+// -nome
+// -genero
+// -ano
+//Depois, exiba a mensagem:
+//O filme Matrix é do gênero Ficção e foi lançado em 1999.
+
+//----------------------------------------------------------------
+
+let filme = {
+  
+    nome: "matrix",
+    genero: "ficção",
+    ano: "1999"
+}
+
+console.log (`O filme ${filme.nome} é do gênero ${filme.genero} e foi lançado em ${filme.ano}.`)
+
+//-----------------------------------------------------------------
+
+//Exercício 8 – Jogo
+//Crie um objeto jogo com:
+// -nome
+// -plataforma
+// -genero
+//Depois, exiba a mensagem:
+//O jogo Minecraft roda na plataforma PC e pertence ao gênero Sandbox.
+
+//------------------------------------------------------------------
+
+let jogo = {
+
+    nome: "Minecraft",
+    plataforma: "Pc",
+    genero: "Sandox"
+}
+
+console.log (`O jogo ${jogo.nome} roda na plataforma ${jogo.plataforma} e pertence ao gênero ${jogo.genero}`)
+
+//-------------------------------------------------------------------
+//Exercício 9 – Conta de Usuário
+//Crie um objeto conta com:
+// -usuario
+// -senha
+// -nivel
+//Depois, exiba a mensagem:
+//O usuário eduardo possui nível administrador e utiliza a senha 1234, muito fraca por sinal.
+//--------------------------------------------------------------------
+
+let Conta = {
+
+    usuario: "eduardo",
+    senha: "1234",
+    nivel: "administrador"
+}
+
+console.log (`O usuário ${Conta.usuario} possui nível ${Conta.nivel} e utiliza a senha ${Conta.senha}, muito fraca por sinal.`)
+
+//-----------------------------------------------------------------------
+
+//Exercício 10 – Escola
+//Crie um objeto escola com:
+// -nome
+// -cidade
+// -turno
+//Depois, exiba a mensagem:
+// A escola EEEP funciona na cidade de Fortaleza no turno da manhã.
+
+//-----------------------------------------------------------------------
+
+let Escola = {
+
+    nome: "EEEP",
+    cidade: "cidade",
+    turno: "manhã"
+}
+
+console.log (`A escola ${Escola.nome} funciona na cidade de ${Escola.cidade} no turno da ${Escola.turno}.`)
